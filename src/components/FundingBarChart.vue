6409f62de5742d466bb4430846db3174bb0c37e5
faaiz <faaizajaz@gmail.com>
2021-02-14 20:38:51 +0500

    Added bar chart component

---
+++ src/components/FundingBarChart.vue
@@ -0,0 +1,19 @@
+<template>
+</template>
+
+<script>
+  import { Bar } from 'vue-chartjs'
+export default {
+
+  name: 'FundingBarChart',
+  extends: Bar,
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
