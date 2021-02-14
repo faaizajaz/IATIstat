239d9a08560eb9ec4bac317459c9df3d2d77a549
faaiz <faaizajaz@gmail.com>
2021-02-14 15:10:27 +0500

    Added initial test API call

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,11 +8,25 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+//import Chart from 'chart.js'
+const axios = require("axios")
 
 export default {
   name: 'Home',
+  data : ()=> {
+    return {
+      labels : [],
+      donors: [],
+    }
+
+  },
   components: {
     HelloWorld
+  },
+  async created() {
+    var row_index = 0;
+    const {data} = await axios.get("https://iatidatastore.iatistandard.org/search/activity/?q=reporting_org_ref:GB-GOV-1&fl=sector,title_narrative&rows=100&start=" + row_index);
+    console.log(data)
   }
 }
 </script>
