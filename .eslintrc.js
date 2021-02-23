2d544b9765d293454c220ee1e5547d01d2f6a967
faaiz <faaizajaz@gmail.com>
2021-02-23 11:24:09 +0500

    Fixed missing comma

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: [
     // add more generic rulesets here, such as:
     // 'eslint:recommended',
+    'plugin:vue/recommended',
     'prettier',
     // 'plugin:vue/recommended' // Use this if you are using Vue.js 2.x.
   ],
