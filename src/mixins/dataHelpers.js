e35d77aee5354e71f35dde7fc1df970e74f26287
faaiz <faaizajaz@gmail.com>
2021-02-28 12:48:15 +0500

    Fixed erros in make_years()

--- src/mixins/dataHelpers.js
+++ src/mixins/dataHelpers.js
@@ -1,4 +1,5 @@
 const isodate = require("isodate");
+const axios = require("axios");
 
 export const dataHelpers = {
   methods: {
@@ -17,5 +18,55 @@ export const dataHelpers = {
       }
       return sum;
     },
+/*    // Options:
+    //    - group sectors into categories
+    make_series: function (raw_data, target_years, transaction_type, options) {
+      // Get the codelist for sector categories
+      let sector_category_codelist_raw;
+      try{
+        axios.get('http://codelists103.archive.iatistandard.org/data/codelist/SectorCategory.json').then(function (data) {
+            sector_category_codelist_raw = data;
+        });
+      } catch (e) {
+        console.log("Could not download sector codelist");
+        console.log(e);
+      }
+
+      // Processing the raw data
+      let trimmed_data = raw_data.data.response.docs ?? {0};
+
+      for (let i = 0; i < trimmed_data.length; i++) {
+        let transaction_values = trimmed_data[i].transaction_value;
+        let transaction_years = make_years(trimmed_data[i].transaction_date);
+
+        try {
+          var curr_sector_code = JSON.parse(trimmed_data[i].sector).sector.code.substring(0, 3);
+        } catch (e) {
+          continue;
+        }
+
+        if (typeof transaction_years !== 'undefined') {
+
+        }
+      }
+    },*/
+
+    make_years: function (dates) {
+      let years = [];
+      if (typeof dates !== 'undefined') {
+        for (let i = 0; i < dates.length; i++) {
+          try {
+            years.push(isodate(dates[i]).getFullYear());
+          } catch (e) {
+            console.log("Couldn't parse dates for activity.");
+            console.log(e);
+            continue;
+          }
+        }
+      }
+      return years;
+    },
+
   },
 };
+
