91afadb23541e2a78260e1f940936da38ad09e4d
faaiz <faaizajaz@gmail.com>
2021-03-07 17:58:59 +0500

    Created table component

---
+++ src/components/Table.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="table">
+    <b>Table is here</b>
+    <vue-good-table v-bind:columns="columns" v-bind:rows="rows"></vue-good-table>
+  </div>
+</template>
+
+<script>
+import 'vue-good-table/dist/vue-good-table.css'
+import { VueGoodTable } from 'vue-good-table'
+
+export default {
+
+  name: 'Table',
+
+  components: {
+    VueGoodTable,
+  },
+
+  props: {
+    columns: {
+      type: Array,
+      default: () => [],
+    },
+    rows: {
+      type: Array,
+      default: () => [],
+    }
+  },
+
+  // Props:
+  //        columns: An array describing columns [ { label: "label1", field "label1" }, {...} ]
+  //        rows: Array containing list with field values [ {id:1, field1: "whatever"}, {...} ]
+  // Apex dataPointSelection event triggers function that creates columns and rows and passes into table component.
+
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
