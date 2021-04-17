4f1ed3e18afa607c0708c854876ef4d1eaa2a908
faaiz <faaizajaz@gmail.com>
2021-04-17 15:02:34 +0500

    Added view to see donor activity in country

---
+++ src/components/CountryByOrgSpending.vue
@@ -0,0 +1,267 @@
+<template>
+  <div>
+    Country by org spending.
+    <v-select multiple :options="org_array" v-model="filter_org"></v-select>
+    <button v-on:click="create_chart">Refresh chart</button>
+    <apexchart type="bar" :options="options" :series="series"></apexchart>
+
+  </div>
+</template>
+
+<script>
+const isodate = require("isodate");
+import { viewHelpers } from "../mixins/viewHelpers.js";
+import { dataHelpers } from "../mixins/dataHelpers.js";
+import 'vue-select/dist/vue-select.css';
+import vSelect  from 'vue-select'
+export default {
+
+  name: 'CountryByOrgSpending',
+  mixins: [viewHelpers, dataHelpers],
+  components: {
+    vSelect
+  },
+  props: {
+    raw_country_data: {
+      type: Object,
+      default: () => null,
+    },
+    target_years: {
+      type: String,
+      default: "",
+    },
+    refresh_chart:{
+      type: Boolean,
+      default: false,
+    }
+  },
+
+  data () {
+    return {
+      options: {
+        chart: {
+          width: "95%",
+          id: "countryorgbarchart",
+          toolbar: {
+            show: true,
+            tools: {
+              selection: true,
+              zoom: true,
+              zoomin: true,
+              zoomout: true,
+              pan: true,
+              reset: true,
+            },
+          },
+          animations: {
+            enabled: false,
+          },
+        },
+        dataLabels: {
+          enabled: false,
+        },
+        plotOptions: {
+          bar: {
+            horizontal: true,
+          },
+        },
+        xaxis: {
+          categories: [],
+          tickPlacement: "on",
+          labels: {
+            formatter: function (value) {
+              let val = (value / 1).toFixed(0).replace(",", ".");
+              return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+            },
+          },
+        },
+        yaxis: {
+          min: 0,
+        },
+
+        noData: {
+          text: "Loading",
+        },
+      },
+      series: [],
+      newcategories: [],
+      target_years_array: [],
+      org_array: [],
+      filter_org: []
+
+    }
+  },
+  watch: {
+    check_for_update: {
+      handler: function () {
+        this.create_chart();
+      }
+    }
+  },
+
+  computed: {
+    check_for_update() {
+      return this.refresh_chart;
+    }
+  },
+
+  created: function () {
+    this.create_chart();
+  },
+
+  methods: {
+    create_chart: function () {
+      this.series = []
+      this.newcategories=[];
+      this.numrecords = 0;
+      this.org_array=[];
+
+      let newseries = {};
+      let activity_list = {};
+
+      this.target_years_array = this.target_years.split(",");
+      for (let year in this.target_years_array) {
+        newseries[this.target_years_array[year]] = [];
+        activity_list[this.target_years_array[year]] = [];
+      }
+
+      if (this.raw_country_data.data !== undefined) {
+        for (let i = 0; i < this.raw_country_data.data.response.docs.length; i++) {
+          var curr_org_code = "Undefined org";
+
+          let curr_transaction_value = this.raw_country_data.data.response.docs[i].transaction_value;
+          let curr_transaction_date=this.raw_country_data.data.response.docs[i].transaction_value_date;
+          let curr_transaction_years = this.make_years(curr_transaction_date);
+
+          try {
+            if (this.raw_country_data.data.response.docs[i].reporting_org_ref !== undefined) {
+              curr_org_code=this.raw_country_data.data.response.docs[i].reporting_org_ref;
+            } else {
+              curr_org_code="Undefined org"
+            }
+          } catch (e) {
+            curr_org_code="Undefined org";
+          }
+
+
+          if (typeof curr_transaction_years!=="undefined") {
+            this.numrecords+=1;
+
+            if (this.org_array.includes(curr_org_code) === false) {
+              this.org_array.push(curr_org_code);
+            }
+
+
+            if (this.newcategories.includes(curr_org_code)) {
+              let a = this.newcategories.indexOf(curr_org_code);
+              for (let year in this.target_years_array) {
+                let q = this.sum_transactions(curr_transaction_value, curr_transaction_date,this.target_years_array[year]);
+
+                let res = newseries[this.target_years_array[year]][a] + q;
+                newseries[this.target_years_array[year]][a] = res;
+
+                if (curr_transaction_years.includes(parseInt(this.target_years_array[year]), 10)) {
+                  try {
+                    activity_list[this.target_years_array[year]][a].push(this.raw_country_data.data.response.docs[i].iati_identifier)
+                  } catch (e) {
+                    console.log(e);
+                  }
+                }
+
+                //Do table stuff here if you want to.
+
+              }
+            } else {
+              if (this.filter_org.length > 0) {
+                if (this.filter_org.includes(curr_org_code)) {
+                  this.newcategories.push(curr_org_code);
+
+                  for (let year in this.target_years_array) {
+                    let q = this.sum_transactions(curr_transaction_value, curr_transaction_date,this.target_years_array[year]);
+
+                    newseries[this.target_years_array[year]].push(q);
+
+                    if (curr_transaction_years.includes(parseInt(this.target_years_array[year]), 10)) {
+                      try {
+                        activity_list[this.target_years_array[year]].push([this.raw_country_data.data.response.docs[i].iati_identifier]);
+                      } catch (e) {
+                        console.log(e);
+                      }
+                    } else {
+                      try {
+                        activity_list[this.target_years_array[year]].push([]);
+                      } catch (e) {
+                        console.log(e);
+                      }
+                    }
+
+                    // DO table stuff here if wanted
+
+
+                  }
+                }
+              } else {
+                this.newcategories.push(curr_org_code);
+
+                for (let year in this.target_years_array) {
+                  let q = this.sum_transactions(curr_transaction_value,curr_transaction_date,this.target_years_array[year]);
+                  newseries[this.target_years_array[year]].push(q);
+
+                  if (curr_transaction_years.includes(parseInt(this.target_years_array[year]), 10)) {
+                    try {
+                      activity_list[this.target_years_array[year]].push([this.raw_country_data.data.response.docs[i].iati_identifier]);
+                    } catch (e) {
+                      console.log(e);
+                    }
+                  } else {
+                    try {
+                      activity_list[this.target_years_array[year]].push([]);
+                    } catch (e) {
+                      console.log(e);
+                    }
+                  }
+                // DO TABLE STUFF HERE IF YOU WANT
+                }
+              }
+            }
+
+          }
+
+        }
+
+        this.options = {
+          ...this.options,
+          ... {
+            xaxis: {
+              categories: this.newcategories,
+            },
+          },
+        };
+
+        try {
+          for (let year in this.target_years_array) {
+            let tmp = {
+              name: this.target_years_array[year],
+              data: newseries[this.target_years_array[year]],
+            };
+            this.series.push(tmp);
+          }
+        } catch (e) {
+          console.log(e);
+        }
+
+        //TABLE STUFF HERE IF WANTED
+
+      } else {
+        console.log("There is no country input data")
+      }
+
+    },
+
+    //AD MAKE TALBE METHOD IF WANTED
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ src/views/CountryByOrg.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <CountryByOrgSpending
+      v-bind:raw_country_data="raw_country_data"
+      v-bind:refresh_chart="refresh_chart"
+      v-bind:target_years="target_years"
+
+    ></CountryByOrgSpending>
+  </div>
+</template>
+
+<script>
+import CountryByOrgSpending from "../components/CountryByOrgSpending";
+
+export default {
+  name: "SpendingByCountryOrg",
+
+  components: {
+    CountryByOrgSpending,
+  },
+
+  props: {
+    raw_country_data: {
+      type: Object,
+      default: () => null,
+    },
+    target_years: {
+      type: String,
+      default: "",
+    },
+    refresh_chart: {
+      type: Boolean,
+      default: false,
+    },
+  },
+
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="css" scoped></style>
