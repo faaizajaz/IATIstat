1254db9876dc2e4e64dd003f3917cd9d2b90fffc
faaiz <faaizajaz@gmail.com>
2021-07-01 17:07:52 +0500

    Added message for IATI API downtime

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <h1>Welcome to the IATI dashboard.</h1>
+    <h3>
+      PLEASE NOTE: The IATI API is temporarily offline for maintenance as of July 1 2021. It is not clear when it will be back online. For more information, see <a href="https://iatistandard.org/en/news/technical-notice-2-iatis-datastore-to-be-integrated-in-new-unified-single-platform/" class="skiplink">the official announcement</a>.
+    </h3>
     <br />
     <h3>
       Organization data is:
